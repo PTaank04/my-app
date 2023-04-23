aae1e76cd88df5a56948f85f4e1c0d8601b67fc8
PTaank04 <107695048+PTaank04@users.noreply.github.com>
2023-04-23 23:37:55 +0100

    native setup with text field

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
+  const handleSearch = async()=>{
+    console.log("Presss")
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{ border: "2px solid black", borderRadius:"4px" , margin:"4px", width:"80%"}}
+        placeholder='IP Address'
+        onChangeText={newText => setText(newText)}
+        value={text}
+      />
+      <Button 
+        title='Search'
+        onPress={()=>handleSearch()}
+        // style={{borderRadius:"9px"}}
+      />
       <StatusBar style="auto" />
     </View>
   );
